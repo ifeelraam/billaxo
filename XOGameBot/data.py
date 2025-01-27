a7eb156ed9ec8bffa59727e5a4c4b5b288e645cb
ifeelraam <aditya20278@gmail.com>
2025-01-27 16:46:19 +0530

    Update data.py

--- XOGameBot/data.py
+++ XOGameBot/data.py
@@ -1,30 +1,49 @@
-from XOGameObject import *
+from XOGameObject import *  # Importing necessary classes and functions from XoGameObject.py
+
+games = []  # List to hold all active games
 
 
-games = []
-
-
-def get_game(game_id: str, player: dict = None) -> XOGame:
+# Function to get an existing game or create a new one
+def get_game(game_id: str, player: dict = None) -> 'XOGame':
+    """
+    Retrieve an existing game or create a new one if it doesn't exist.
+    :param game_id: The unique game ID.
+    :param player: Player details if the game is newly created.
+    :return: The game object.
+    """
     for game in games:
         if game.game_id == game_id:
             return game
-    games.append(XOGame(game_id, player))
+    games.append(XOGame(game_id, player))  # Create a new game if not found
     return games[-1]
 
 
+# Function to remove a game by its ID
 def remove_game(game_id: str) -> bool:
+    """
+    Removes a game from the list by its unique game ID.
+    :param game_id: The unique game ID.
+    :return: True if the game was removed, False otherwise.
+    """
     for index in range(len(games)):
         if games[index].game_id == game_id:
-            games.pop(index)
+            games.pop(index)  # Remove the game from the list
             return True
     return False
 
 
-def reset_game(game: XOGame) -> XOGame:
+# Function to reset the game
+def reset_game(game: 'XOGame') -> 'XOGame':
+    """
+    Rᴇsᴇᴛs ᴀɴ ᴇxɪsᴛɪɴɢ ɢᴀᴍᴇ ʙʏ ᴄʀᴇᴀᴛɪɴɢ ᴀ ɴᴇᴡ ᴏɴᴇ ᴡɪᴛʜ ᴛʜᴇ sᴀᴍᴇ ᴘʟᴀʏᴇʀs.
+    :param game: The game to reset.
+    :return: A new game instance with the same players and game ID.
+    """
     temp_game_id = game.game_id
-    temp_p1 = game.player2
-    temp_p2 = game.player1
+    temp_p1 = game.player1  # Save player 1
+    temp_p2 = game.player2  # Save player 2
 
-    if remove_game(game.game_id):
-        games.append(XOGame(temp_game_id, temp_p1, temp_p2))
-        return games[-1]
+    if remove_game(game.game_id):  # Remove the existing game
+        games.append(XOGame(temp_game_id, temp_p1, temp_p2))  # Add a new game with same players
+        return games[-1]  # Return the newly created game
+    return None  # Return None if no game was reset
