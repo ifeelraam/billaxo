939e9438e2dc082aa87083621ac1f838f257e06b
ifeelraam <aditya20278@gmail.com>
2025-01-27 17:01:43 +0530

    Update emojis.py

--- XOGameBot/emojis.py
+++ XOGameBot/emojis.py
@@ -1,13 +1,15 @@
-X = u"\U0000274C"
-O = u"\U00002B55"
-X_loser = u"\U00002716"
-O_loser = u"\U0001F518"
-vs = u"\U0001F19A"
-game = u"\U0001F3AE"
-trophy = u"\U0001F3C6"
-draw = u"\U0001F3C1"
-swords = u"\U00002694\U0000FE0F"
-id = u"\U0001F194"
-mail = u"\U00002709"
-cat = u"\U0001F63A"
-back = u"\U000025C0"
+# emojis.py
+
+X = u"\U0000274C"           # Cross emoji (X)
+O = u"\U00002B55"           # Circle emoji (O)
+X_loser = u"\U00002716"     # Cross emoji (X) - loser
+O_loser = u"\U0001F518"     # Circle emoji (O) - loser
+vs = u"\U0001F19A"          # VS emoji (for match-up)
+game = u"\U0001F3AE"        # Game controller emoji
+trophy = u"\U0001F3C6"      # Trophy emoji (winner)
+draw = u"\U0001F3C1"        # Flag emoji (draw)
+swords = u"\U00002694\U0000FE0F"  # Crossed swords emoji
+id = u"\U0001F194"          # ID card emoji
+mail = u"\U00002709"        # Mailbox emoji
+cat = u"\U0001F63A"         # Cat face emoji
+back = u"\U000025C0"        # Back arrow emoji
